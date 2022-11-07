e1a60cff870f0ede44268606f944bd973d7cfbae
dongilK <dongil.kim@powertask.biz>
2022-11-07 16:56:25 +0900

    :tada: Initialize Frontend - react + typescript + tailwind

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'avoid',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  tabWidth: 2,
+  printWidth: 80,
+}
+
+module.export = options
