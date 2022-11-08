ffff840a630f944d76cc66af132f4dd8ae9c7b96
dongilK <dongil.kim@powertask.biz>
2022-11-08 18:29:11 +0900

    :hammer: add responsive navbar

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,13 @@
 const options = {
-  arrowParens: 'avoid',
+  arrowParens: "avoid",
+  bracketSpacing: false,
   semi: false,
+  htmlWhitespaceSensitivity: "css",
   singleQuote: true,
-  trailingComma: 'all',
-  endOfLine: 'lf',
+  trailingComma: "all",
+  endOfLine: "lf",
   tabWidth: 2,
   printWidth: 80,
-}
+};
 
-module.export = options
+module.export = options;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+type NavbarProps = {
+  //
+};
+
+export const Navbar = (props: NavbarProps) => {
+  /** Property */
+  const { ...others } = props;
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const menuLinks = [
+    {
+      id: 0,
+      title: "home",
+    },
+    {
+      id: 1,
+      title: "about",
+    },
+    {
+      id: 2,
+      title: "portfolio",
+    },
+    {
+      id: 3,
+      title: "experience",
+    },
+    {
+      id: 4,
+      title: "contact",
+    },
+  ];
+
+  /** Function */
+  console.log(isNavOpen);
+
+  /** Render */
+  return (
+    <div className="flex justify-between items-center px-4 text-white bg-black w-full h-20 fixed">
+      <div>
+        <h1 className="text-2xl font-signature ml-2">DONGIL</h1>
+      </div>
+
+      <ul className="hidden md:flex">
+        {menuLinks.map((link) => (
+          <li
+            key={link.id}
+            className="px-4 text-gray-500 font-medium capitalize cursor-pointer hover:scale-105 duration-300"
+          >
+            {link.title}
+          </li>
+        ))}
+      </ul>
+
+      <div className="text-gray-500 pr-4 z-10 cursor-pointer md:hidden">
+        <button onClick={() => setIsNavOpen((prev) => !prev)}>
+          {isNavOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+        </button>
+      </div>
+      {isNavOpen && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {menuLinks.map((link) => (
+            <li
+              key={link.id}
+              className="px-4 py-6 text-4xl capitalize cursor-pointer hover:scale-105 duration-300"
+            >
+              {link.title}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
